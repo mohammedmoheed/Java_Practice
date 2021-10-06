b5ed2ced8e1c3f97bcf4329d4e2101e6e51cd91c
mohammedmoheed <moheed9511@gmail.com>
2021-10-06 18:08:05 +0530

    cyclic sort and some soring questions

--- src/com/Sorting/Cyclic_sort.java
+++ src/com/Sorting/Cyclic_sort.java
@@ -2,21 +2,21 @@ package com.Sorting;
 
 import java.util.Arrays;
 
-public class Insertion_sort {
+public class Cyclic_sort {
     public static void main(String[] args) {
-        int[] arr = {5,4,2,1,3};
-        Insertion_Sort(arr);
+        int[] arr = {3,4,2,1,3};
+        cyclic_sort(arr);
         System.out.println(Arrays.toString(arr));
-
-
     }
-    static void Insertion_Sort(int[] arr){
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j >0 ; j--) {
-                if (arr[j] < arr[j-1]) {
-                    swap(arr,j,j-1);
-                }
+
+    static void cyclic_sort(int[] arr){
+        int i = 0;
+        while(i< arr.length){
+            int correct = arr[i]-1;
+            if (arr[i] != arr[correct] ) {
+                swap(arr, i, correct);
             }
+            else i++;
         }
     }
     static void swap(int[] arr, int first, int last) {
